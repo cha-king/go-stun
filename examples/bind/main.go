0f8f35fba01a12cc44ec0a09864ffbac2d55fc20
Charlie King <charlie.king@verkada.com>
2022-06-29 23:11:42 -0700

    Add bind example

---
+++ examples/bind/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/cha-king/go-stun"
+)
+
+const stunUrl = "stun.l.google.com:19302"
+
+func main() {
+	addr, err := net.ResolveUDPAddr("udp4", stunUrl)
+	if err != nil {
+		panic(err)
+	}
+
+	remoteAddr, err := stun.Bind(addr)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(remoteAddr)
+}
