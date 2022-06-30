0ada2644dd9e5057d72af6476b60685de9d9a0f8
Charlie King <charlie.king@verkada.com>
2022-06-29 23:11:27 -0700

    Add Bind method

--- stun.go
+++ stun.go
@@ -1,62 +1,48 @@
-package main
+package stun
 
 import (
-	"crypto/rand"
-	"encoding/binary"
+	"errors"
+	"net"
 )
 
-const (
-	MessageClassRequest    uint8 = 0b00
-	MessageClassIndication uint8 = 0b01
-	MessageClassSuccess    uint8 = 0b10
-	MessageClassError      uint8 = 0b11
-)
-
-const (
-	MessageMethodBinding uint8 = 0x0001
-)
-
-const MessageMagicCookie uint32 = 0x2112A442
-
-const transactionIdLength = 12
-
-type header struct {
-	class         uint8
-	method        uint8
-	length        uint16
-	transactionId [transactionIdLength]byte
-}
-
-func (h header) encode() []byte {
-	output := make([]byte, 20)
-	class := uint16(h.class)
-
-	classBytes := (class << 4 & 0x0F0) | (class << 8 & 0xF00)
-
-	var methodBytes uint16
-	methodBytes |= uint16(h.method) & 0x000F
-	methodBytes |= uint16(h.method) << 1 & 0x00F0
-	methodBytes |= uint16(h.method) << 2 & 0xFF00
-
-	typeBytes := classBytes | methodBytes
-
-	binary.BigEndian.PutUint16(output[0:2], typeBytes)
-	binary.BigEndian.PutUint16(output[2:4], h.length)
-	binary.BigEndian.PutUint32(output[4:8], MessageMagicCookie)
-	binary.BigEndian.PutUint32(output[8:12], binary.BigEndian.Uint32(h.transactionId[0:4]))
-	binary.BigEndian.PutUint32(output[12:16], binary.BigEndian.Uint32(h.transactionId[4:8]))
-	binary.BigEndian.PutUint32(output[16:20], binary.BigEndian.Uint32(h.transactionId[8:12]))
-
-	return output
-}
-
-func newHeader(class uint8, method uint8) header {
-	var transactionId [transactionIdLength]byte
-	randBytes := make([]byte, transactionIdLength)
-	_, err := rand.Read(randBytes)
+func Bind(serverAddr *net.UDPAddr) (net.UDPAddr, error) {
+	conn, err := net.DialUDP("udp", nil, serverAddr)
 	if err != nil {
-		panic(err)
+		return net.UDPAddr{}, err
 	}
-	copy(transactionId[:], randBytes)
-	return header{class, method, 0, transactionId}
+	defer conn.Close()
+
+	m := newMessage(MessageClassRequest, MessageMethodBinding, nil)
+
+	_, err = conn.Write(m.encode())
+	if err != nil {
+		return net.UDPAddr{}, err
+	}
+
+	buf := make([]byte, 256)
+	_, err = conn.Read(buf)
+	if err != nil {
+		return net.UDPAddr{}, err
+	}
+
+	r, err := decodeMessage(buf)
+	if err != nil {
+		return net.UDPAddr{}, err
+	}
+
+	if m.transactionId != r.transactionId {
+		return net.UDPAddr{}, errors.New("transaction id mismatch")
+	}
+
+	attr := r.attributes[0]
+	mAddr, ok := attr.(xorMappedAddress)
+	if !ok {
+		return net.UDPAddr{}, errors.New("unsupported attribute")
+	}
+	addr, err := mAddr.getAddress()
+	if err != nil {
+		return net.UDPAddr{}, err
+	}
+
+	return addr, nil
 }
