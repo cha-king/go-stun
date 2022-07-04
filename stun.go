bb11d357ce5ef252927f36c8d211d75805bf25fe
Charlie King <charlie.king@verkada.com>
2022-07-04 16:04:18 -0700

    Create stun client

--- stun.go
+++ stun.go
@@ -5,24 +5,27 @@ import (
 	"net"
 )
 
-func Bind(serverAddr *net.UDPAddr) (net.UDPAddr, error) {
-	conn, err := net.DialUDP("udp", nil, serverAddr)
-	if err != nil {
-		return net.UDPAddr{}, err
-	}
-	defer conn.Close()
+type Client struct {
+	conn *net.UDPConn
+	addr *net.UDPAddr
+}
 
+func (c *Client) Bind() (net.UDPAddr, error) {
 	m := newMessage(MessageClassRequest, MessageMethodBinding, nil)
 
-	_, err = conn.Write(m.encode())
+	_, err := c.conn.WriteToUDP(m.encode(), c.addr)
 	if err != nil {
 		return net.UDPAddr{}, err
 	}
 
+	// TODO: Discard stun messages from unknown senders rather than error
 	buf := make([]byte, 256)
-	_, err = conn.Read(buf)
+	_, remoteAddr, err := c.conn.ReadFromUDP(buf)
 	if err != nil {
 		return net.UDPAddr{}, err
+	}
+	if !compareUdpAddr(remoteAddr, c.addr) {
+		return net.UDPAddr{}, errors.New("unknown sender")
 	}
 
 	r, err := decodeMessage(buf)
@@ -45,4 +48,21 @@ func Bind(serverAddr *net.UDPAddr) (net.UDPAddr, error) {
 	}
 
 	return addr, nil
+}
+
+func NewClient(conn *net.UDPConn, addr *net.UDPAddr) *Client {
+	return &Client{conn, addr}
+}
+
+func compareUdpAddr(base *net.UDPAddr, ref *net.UDPAddr) bool {
+	if !base.IP.Equal(ref.IP) {
+		return false
+	}
+	if base.Port != ref.Port {
+		return false
+	}
+	if base.Zone != ref.Zone {
+		return false
+	}
+	return true
 }
