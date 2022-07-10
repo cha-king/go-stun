ea393d5b8e2cdb3e2521a86c3dbdc1b645a829f0
Charlie King <charlie.king@verkada.com>
2022-07-09 18:29:19 -0700

    Restore basic bind example

--- examples/bind/main.go
+++ examples/bind/main.go
@@ -3,38 +3,11 @@ package main
 import (
 	"fmt"
 	"net"
-	"time"
 
 	"github.com/cha-king/go-stun"
 )
 
 const stunUrl = "stun.l.google.com:19302"
-
-type fakeConn struct {
-	conn net.PacketConn
-}
-
-func (c *fakeConn) ReadFrom(p []byte) (n int, addr net.Addr, err error) {
-	return c.conn.ReadFrom(p)
-}
-func (c *fakeConn) WriteTo(p []byte, addr net.Addr) (n int, err error) {
-	return c.conn.WriteTo(p, addr)
-}
-func (c *fakeConn) Close() error {
-	return c.conn.Close()
-}
-func (c *fakeConn) LocalAddr() net.Addr {
-	return c.conn.LocalAddr()
-}
-func (c *fakeConn) SetDeadline(t time.Time) error {
-	return c.conn.SetDeadline(t)
-}
-func (c *fakeConn) SetReadDeadline(t time.Time) error {
-	return c.conn.SetReadDeadline(t)
-}
-func (c *fakeConn) SetWriteDeadline(t time.Time) error {
-	return c.conn.SetWriteDeadline(t)
-}
 
 func main() {
 	addr, err := net.ResolveUDPAddr("udp4", stunUrl)
@@ -48,9 +21,7 @@ func main() {
 	}
 	defer conn.Close()
 
-	mConn := &fakeConn{conn: conn}
-
-	client := stun.NewClient(mConn)
+	client := stun.NewClient(conn)
 
 	remoteAddr, err := client.BindRequest(addr)
 	if err != nil {
